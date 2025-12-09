42b41f508986f0b031f868094ef18ef7b2c3fefa
Zack Zhou <yzhou50@gmail.com>
2025-12-08 21:59:38 -0600

    v2 using pygame

--- loud_keyboard.py
+++ loud_keyboard.py
@@ -1,84 +1,33 @@
-from fileinput import filename
-from random import randint
-from pynput.keyboard import Key, Listener
-# import required module
+import pygame
+import random
+import os
 
+# --- Setup pygame audio ---
+pygame.mixer.init()
 
-from playsound import playsound
+# Path to your folder of mp3s
+SOUND_DIR = "keyboard_sounds"
 
-import pyglet
+# Load all sound files from 1.mp3 to 10.mp3
+sounds = []
+for i in range(1, 11):
+    filepath = os.path.join(SOUND_DIR, f"{i}.mp3")
+    sounds.append(pygame.mixer.Sound(filepath))
 
-pyglet.resource.path = ['.\\keyboard_sounds\\']
-pyglet.resource.reindex()
+# --- Setup pygame window for key events ---
+pygame.init()
+screen = pygame.display.set_mode((300, 200))
+pygame.display.set_caption("Press any key to play a sound")
 
+running = True
+while running:
+    for event in pygame.event.get():
+        # Quit on window close
+        if event.type == pygame.QUIT:
+            running = False
 
-def play_random_sound():
-    filename = str(randint(2, 9)) + '.mp3'
-    pyglet.resource.media(filename, streaming=False).play()
+        # Play a random sound on keypress
+        if event.type == pygame.KEYDOWN:
+            random.choice(sounds).play()
 
-
-def play_specific_sound(filename):
-    pyglet.resource.media(filename, streaming=False).play()
-
-
-# keyboard_sounds\3.mp3
-# C:\Users\ZackZhou\local\loud_keyboard\keyboard_sounds\1.mp3
-# for playing note.mp3 file
-# playsound('/path/note.mp3')
-# print('playing sound using  playsound')
-
-
-def on_press(key):
-    print('{0} pressed'.format(
-        key))
-    play = True
-    play2 = False
-    play3 = False
-    if key == Key.ctrl_l:
-        print("don't play sounds for keys that need to be held")
-        play = False
-    if key == Key.shift_l:
-        print("don't play sounds for keys that need to be held")
-        play = False
-    if key == Key.ctrl_r:
-        print("don't play sounds for keys that need to be held")
-        play = False
-    if key == Key.shift_r:
-        print("don't play sounds for keys that need to be held")
-        play = False
-    if key == Key.cmd:
-        print("don't play sounds for keys that need to be held")
-        play = False
-    if key == Key.space:
-        play = False
-        play2 = True
-    if key == Key.enter:
-        play = False
-        play2 = True
-    if key == Key.backspace:
-        play = False
-        play3 = True
-    if play:
-        play_random_sound()
-    if play2:
-        play_specific_sound("1.mp3")
-    if play3:
-        play_specific_sound("10.mp3")
-
-
-def on_release(key):
-    print('{0} release'.format(
-        key))
-    if key == Key.end:
-        # Stop listener
-        return False
-
-
-# Collect events until released
-with Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
-
-if __name__ == "__main__":
-    pyglet.app.run()
+pygame.quit()
